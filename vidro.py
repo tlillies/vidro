2a473b37ecfc791acce98fd77bd2494823538577
tlillies <thomasglillis@gmail.com>
2014-09-14 15:45:11 -0600

    atan2 and other updates

--- vidro.py
+++ vidro.py
@@ -673,7 +673,9 @@ def rc_go_to_alt(goal_alt):
 
 	#Send RC value
 	curses_print("T: "+ str(int(1370+error_alt*alt_K_P+I_error_alt*alt_K_I)) + " = 1370 + " + str(error_alt*alt_K_P) + " + " + str(I_error_alt*alt_K_I), 19, 0)
-
+	
+	rc_throttle(1370+error_alt*alt_K_P+I_error_alt*alt_K_I)
+	
 	return error_alt
 
 def rc_go_to_heading(goal_heading):
@@ -728,9 +730,6 @@ def rc_go_to_xy(goal_x, goal_y):
 	global D_error_pitch
 	global I_error_roll
 	global I_error_pitch
-	global sum_lat
-	global sum_lon
-	global count_lat_lon
 	global error_roll
 	global error_pitch
 	global x_current
@@ -744,36 +743,20 @@ def rc_go_to_xy(goal_x, goal_y):
 	global error_x
 	global error_y
 
-	#Average out lat and lon (May not be needed. Need to text this)
-	sum_lat += get_lat()
-	sum_lon += get_lon()
-	count_lat_lon += 1
-	average_lat = 0.0
-	average_lon = 0.0
-	if count_lat_lon != 1:
-		return
-	average_lat = sum_lat / 1.0
-	average_lon = sum_lon / 1.0
-	sum_lat = 0
-	sum_lon = 0
-	count_lat_lon = 0
-
 	#Get current heading for shifting axis
 	heading = get_yaw_degrees()
 
 	#Calculate current position (Need to find which one works best)
-	#x_current = calc_sitl_distance_x()
-	#y_current = calc_sitl_distance_y()
-	x_current = calc_sitl_distance(home_lat, home_lon, home_lat, average_lon)
-	y_current = calc_sitl_distance(home_lat, home_lon, average_lat, home_lon)
-	#x_current = calc_utm_distance(home_lat, home_lon, home_lat, get_lon())
-	#y_current = calc_utm_distance(home_lat, home_lon, get_lat(), home_lon)
+	x_current = get_position()[0]
+	y_current = get_position()[1]
+
 
 	#Assign distance with appropriate sign
-	if get_lat() < home_lat:
-		y_current *= -1
-	if get_lon() < home_lon:
-		x_current *= -1
+	if sitl == True:
+		if get_lat() < home_lat:
+			y_current *= -1
+		if get_lon() < home_lon:
+			x_current *= -1
 
 	#Calculate the error in the x-y(lat/lon) axis
 	error_x = goal_x - x_current * 1.0
@@ -790,15 +773,7 @@ def rc_go_to_xy(goal_x, goal_y):
 	total_error = math.sqrt(error_x*error_x+error_y*error_y)
 
 	#Angle on x-y(lat/lon) axis to point
-	waypoint_angle = math.degrees(math.atan(error_y/error_x))
-
-	#Put angle in the correct quadrant
-	if error_x < 0 and error_y < 0:
-		waypoint_angle += 180
-	if error_x < 0 and error_y > 0:
-		waypoint_angle += 180
-	if error_x > 0 and error_y < 0:
-		waypoint_angle += 360
+	waypoint_angle = math.degrees(math.atan2(error_y,error_x))
 
 	#Calculate the offset of the vehicle from the x-y (lat-lon) axis
 	vehicle_angle = 90 - (waypoint_angle + heading)
@@ -817,8 +792,6 @@ def rc_go_to_xy(goal_x, goal_y):
 	curses_print("Roll Error: " + str(round(error_roll)), 13, 1)
 	curses_print("Pitch Error: " + str(round(error_pitch)), 13, 0)
 	curses_print("Total Error: " + str(round(total_error)), 16, 0)
-	#curses_print("Lat, Lon: " + str(home_lat) + ", " + str(home_lon), 18)
-	#curses_print("Lat, Lon: " + str(average_lat) + ", " + str(average_lon), 19)
 
 
 	#Calculate delta-t for integration
@@ -841,8 +814,8 @@ def rc_go_to_xy(goal_x, goal_y):
 	curses_print("R: " +  str(int(1540+error_roll*roll_K_P+I_error_roll*roll_K_I+D_error_roll*roll_K_D)) + " = 1540 + " + str(error_roll*roll_K_P) + " + " + str(I_error_roll*roll_K_I) + " + " + str(D_error_roll*roll_K_D), 22, 0)
 
 	#Send RC values
-	rc_pitch( 1540 + (error_pitch*pitch_K_P) + (I_error_pitch*pitch_K_I) + (D_error_pitch*pitch_K_D) )
-	rc_roll(  1540 + (error_roll*roll_K_P) + (I_error_roll*roll_K_I) + (D_error_roll*roll_K_D) )
+	rc_pitch( 1550 + (error_pitch*pitch_K_P) + (I_error_pitch*pitch_K_I) + (D_error_pitch*pitch_K_D) )
+	rc_roll(  1550 + (error_roll*roll_K_P) + (I_error_roll*roll_K_I) + (D_error_roll*roll_K_D) )
 
 
 
@@ -862,21 +835,21 @@ timer = time.clock()
 
 #Main program loop
 while v.channel_readback['6'] < 1100:
-
+	screen.erase()
 
 	#Set the gains from the gain file
 	get_gains()
 
 
 	#Setting goal
-	rc_go_to_alt(10000)
+	rc_go_to_alt(500)
 	"""
 	yaw_error = rc_go_to_heading(.78539816)
 	if yaw_error < .1 and yaw_error > -.1:
 		rc_go_to_xy(1000, 1000)
 	"""
-	rc_go_to_heading(.78539816)
-	rc_go_to_xy(1000, 1000)
+	rc_go_to_heading(0)
+	rc_go_to_xy(0, 0)
 
 	#Add values to arrays for plotting
 	plot_error_yaw.append(error_yaw)
