3ecc18f7a67443c6e05e42e0de34377c57abb68e
tlillies <thomasglillis@gmail.com>
2014-10-15 14:38:08 -0600

    only reset gains when entering program control loop

--- tests/plot_test.py
+++ tests/plot_test.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.figure(1)
-#plt.axis([0, 1000, 0, 1])
-#plt.ion()
+plt.axis([0, 1000, 0, 1])
+plt.ion()
 
 for i in range(100):
     y = np.random.random()
