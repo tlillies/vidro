29ac09f922237a2e5ac8c2aa45471e2ab415a394
tlillies <thomasglillis@gmail.com>
2014-11-11 15:43:34 -0700

    organized

--- deprecated/SSH_GCS/ssh_gcs.sh
+++
@@ -1,2 +0,0 @@
-cd ~/
-sudo mavproxy.py --master=/dev/ttyUSB0 --baudrate=57600 --aircraft="SSH_GCS"
--- deprecated/current/plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plot
-
-plot.figure(1)
-plot.plot([1,2,3,4],[1,4,9,16])
-plot.figure(2)
-plot.plot([1,2,3,4],[7,4,9,16])
-plot.show()
