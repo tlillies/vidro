cf411e75ccedcf7db8d5c61e761d57da8b3b5834
thomas lillis <thomasglillis@gmail.com>
2014-11-14 21:29:22 +0000

    convert to hsv and minimal color detection

--- tests/camera_thread.py
+++ tests/camera_thread.py
@@ -5,11 +5,14 @@ import picamera
 import picamera.array
 import curses
 import matplotlib.pyplot as plot
+import numpy as np
 
 plot_camera_time=[]
 plot_camera_cycle=[]
+plot_camera_cycle_number=[]
 plot_main_time=[]
 plot_main_cycle=[]
+plot_main_cycle_number=[]
 
 def curses_print(string, line, col):
     """
@@ -38,25 +41,34 @@ screen.refresh()
 
 frame = None
 
+new_frame = True
+
 def camera(event):
     global frame
+    global new_frame
     global plot_camera_time
     global Plot_camera_cycle
     global start_time
     previous_time = time.time()
+    cycle = 0
     with picamera.PiCamera() as camera:
         with picamera.array.PiRGBArray(camera) as stream:
             camera.resolution = (320, 240)
+            camera.hflip = True
+            camera.vflip = True
 
             while not event.is_set():
                 current_time = time.time()
                 plot_camera_time.append(current_time-start_time)
                 plot_camera_cycle.append(current_time-previous_time)
+                plot_camera_cycle_number.append(cycle)
+                cycle += 1
                 previous_time = current_time
                 curses_print("In camera loop",3,0)
                 camera.capture(stream, 'bgr', use_video_port=True)
                 # stream.array now contains the image data in BGR order
 		frame = stream.array
+                new_frame = True
                 stream.truncate(0)
 
 event = threading.Event()
@@ -68,21 +80,46 @@ d.start()
 start_time = time.time()
 previous_time = time.time()
 
+cycle = 0
+
 while (time.time()-start_time) < 15:
     current_time = time.time()
     curses_print(str(current_time-start_time),0,0)
+    curses_print("New Frame: " + str(new_frame),7,0)
     plot_main_time.append(current_time-start_time)
     plot_main_cycle.append((current_time-previous_time))
+    plot_main_cycle_number.append(cycle)
+    cycle += 1
     previous_time = current_time
-    """
-    try:
-        cv2.imshow('frame', frame)
-    except:
-        pass
-        curses_print("Failed image show",1,0)
-    """
+    if new_frame == True:
+        """
+        try:
+            cv2.imshow('frame', frame)
+        except:
+            curses_print("Failed image show",1,0)
+        """
+        try:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            lower_blue = np.array([110, 50, 50], dtype=np.uint8)
+            upper_blue = np.array([130,255,255], dtype=np.uint8)
+
+            # Threshold the HSV image to get only blue colors
+            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+            # Bitwise-AND mask and original image
+            res = cv2.bitwise_and(frame,frame, mask= mask)
+        
+            #cv2.imshow('frame',frame)
+            #cv2.imshow('mask',mask)
+            cv2.imshow('res',res)
+            new_frame = False
+        except:
+            pass
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
     screen.clear()
     screen.refresh()
 
@@ -98,5 +135,13 @@ plot.ylabel("Time/cycle")
 plot.title("Thread and main loop cycle times")
 plot.plot(plot_camera_time, plot_camera_cycle)
 plot.plot(plot_main_time, plot_main_cycle)
+
+plot.figure(2)
+plot.xlabel("Loop cycle")
+plot.ylabel("Time/cycle")
+plot.title("Thread and main loop cycle times")
+plot.plot(plot_camera_cycle_number, plot_camera_cycle)
+plot.plot(plot_main_cycle_number, plot_main_cycle)
+
 
 plot.show()
