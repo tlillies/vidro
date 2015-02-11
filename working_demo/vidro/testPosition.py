8ff8d45baf2d2ccb08abae48d766bac32f37c0ee
tlillies <thomasglillis@gmail.com>
2015-02-10 21:37:37 -0700

    added vicon position with vrpn

---
+++ working_demo/vidro/testPosition.py
@@ -0,0 +1,22 @@
+import Position
+import signal
+import sys
+import time
+
+v = 0
+def signal_handler(signal, frame):
+    global v
+    print('Exiting controller')
+    v.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+v = Position.ViconPosition("test1@Vicon")
+v.start()
+while (1):
+    print 'Z: %1.4f' % v.position[2]
+    #time.sleep(1)
+
+
+
+
---
+++ working_demo/vidro/vicon_stream_test.py
@@ -0,0 +1,26 @@
+import transformations
+import vrpn
+import math
+import time
+
+def callback(userdata, packet):
+	#print packet
+    quat = packet['quaternion']
+    fixed_axes = (quat[3], quat[0], quat[1], quat[2])
+        
+    #print fixed_axes
+    angles = transformations.euler_from_quaternion(fixed_axes, 'rxyz')
+    #print angles
+    rad2deg = 180 / math.pi
+        
+    position = packet['position']
+    pos_angles = (angles[1]*rad2deg, angles[0]*rad2deg, angles[2]*rad2deg)
+    print "CB: X: %f \tY: %f \tZ: %f \tRoll: %f \tPitch: %f \tYaw: %f" % \
+         (position[0], position[1], position[2], pos_angles[0], pos_angles[1], pos_angles[2])
+	
+	
+#print(userdata, " => ", data);
+tracker=vrpn.receiver.Tracker("test1@vicon")
+tracker.register_change_handler("position", callback, "position")
+while 1:
+    tracker.mainloop()
