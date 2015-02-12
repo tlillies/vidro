f396dad417a58e97a9106f83c15194521ff7e164
tlillies <thomasglillis@gmail.com>
2015-02-11 20:11:21 -0700

    added new streaming to vidro

--- working_demo/vidro/testPosition.py
+++ working_demo/vidro/testPosition.py
@@ -1,21 +1,26 @@
-import Position
+import vicon
 import signal
 import sys
 import time
 
-v = 0
 def signal_handler(signal, frame):
-    global v
+    global quad
+    global wand
     print('Exiting controller')
-    v.stop()
+    quad.stop()
+    wand.stop()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
-v = Position.ViconPosition("test1@Vicon")
-v.start()
+quad = vicon.ViconPosition("pixhawk(new)@Vicon")
+quad.start()
+
+wand = vicon.ViconPosition("Wand@Vicon")
+wand.start()
+
 while (1):
-    print 'Z: %1.4f' % v.position[2]
-    #time.sleep(1)
+    print "QUAD" + str(quad.position)
+    print "WAND" + str(wand.position)
 
 
 
--- working_demo/vidro/vicon_stream_test.py
+++ working_demo/vidro/vicon_stream_test.py
@@ -3,7 +3,7 @@ import vrpn
 import math
 import time
 
-def callback(userdata, packet):
+def callback_quad(userdata, packet):
 	#print packet
     quat = packet['quaternion']
     fixed_axes = (quat[3], quat[0], quat[1], quat[2])
@@ -15,14 +15,29 @@ def callback(userdata, packet):
         
     position = packet['position']
     pos_angles = (angles[1]*rad2deg, angles[0]*rad2deg, angles[2]*rad2deg)
-    print "CB: X: %f \tY: %f \tZ: %f \tRoll: %f \tPitch: %f \tYaw: %f" % \
+    print "QUAD: X: %f \tY: %f \tZ: %f \tRoll: %f \tPitch: %f \tYaw: %f" % \
+         (position[0], position[1], position[2], pos_angles[0], pos_angles[1], pos_angles[2])
+      
+def callback_wand(userdata, packet):
+	#print packet
+    quat = packet['quaternion']
+    fixed_axes = (quat[3], quat[0], quat[1], quat[2])
+        
+    #print fixed_axes
+    angles = transformations.euler_from_quaternion(fixed_axes, 'rxyz')
+    #print angles
+    rad2deg = 180 / math.pi
+        
+    position = packet['position']
+    pos_angles = (angles[1]*rad2deg, angles[0]*rad2deg, angles[2]*rad2deg)
+    print "WAND: X: %f \tY: %f \tZ: %f \tRoll: %f \tPitch: %f \tYaw: %f" % \
          (position[0], position[1], position[2], pos_angles[0], pos_angles[1], pos_angles[2])
 	
-quad=vrpn.receiver.Tracker("quad@vicon")
-quad.register_change_handler("position", callback, "position")
+quad=vrpn.receiver.Tracker("pixhawk(new)@vicon")
+quad.register_change_handler("position", callback_quad, "position")
 
-wand=vrpn.receiver.Tracker("wand@vicon")
-wand.register_change_handler("position", callback, "position")
+wand=vrpn.receiver.Tracker("Wand@vicon")
+wand.register_change_handler("position", callback_wand, "position")
 
 while 1:
     quad.mainloop()
